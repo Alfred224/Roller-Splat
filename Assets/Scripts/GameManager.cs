fc0e60bf7ed5a9d24dec650c732b2507a777e53e
Alfred224 <97020796+Alfred224@users.noreply.github.com>
2022-10-18 10:24:52 -0700

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager singleton;
+
+    private GroundPiece[] allGroundPieces;
+
+    private void Start()
+    {
+        SetupNewLevel();
+    }
+
+    private void SetupNewLevel()
+    {
+        allGroundPieces = FindObjectsOfType<GroundPiece>();
+    }
+
+    private void Awake()
+    {
+        if (singleton == null)
+            singleton = this;
+        else if (singleton != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        SetupNewLevel();
+    }
+
+    public void CheckComplete()
+    {
+        bool isFinished = true;
+
+        for (int i = 0; i < allGroundPieces.Length; i++)
+        {
+            if (allGroundPieces[i].isColored == false)
+            {
+                isFinished = false;
+                break;
+            }
+        }
+
+        if (isFinished)
+            NextLevel();
+    }
+
+    private void NextLevel()
+    {
+        // Little adjustment to make the game restart to level 1 when there are no more levels
+        SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings > SceneManager.GetActiveScene().buildIndex + 1 ? SceneManager.GetActiveScene().buildIndex + 1 : 0);
+    }
+
+}
